37704e3ff4363143b1eab5076e8d596203e016c3
Hamzah <hamzalianwar@hotmail.com>
2024-12-24 02:11:57 -0500

    Day4 Challenge

---
+++ Day_4.py
@@ -0,0 +1,23 @@
+# Rock, Paper & Scissors
+# Rules: Rock wins against scissors, Scissors win against paper, Paper wins against rock
+ 
+import random
+
+user_choice = input("Please choose 0 for Rock, 1 for Paper & 2 for Scissors")
+
+computer_choice = random.randint(0, 2)
+print(f"Computer chose: {computer_choice}")
+
+if user_choice >= "3" or user_choice < "0":
+    print("You typed an invalid number, you lose!")
+elif user_choice == "0" and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == "2":
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw!")
+
