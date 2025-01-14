d118fe2a216f4d0c996ed66a08150fd3c0f36975
Hamzah <hamzalianwar@hotmail.com>
2025-01-14 14:56:06 -0500

    Modified Day four code

--- 100DaysChallenges/dayFour.py
+++ 100DaysChallenges/dayFour.py
@@ -1,11 +1,29 @@
 # Strings are immutable
 # upper, lower, rstrip, replace, split, capitalize, center, count, endwith, find, isalnum, isalpha, islower,
 # isprintable, isspace, istitle, startswith, swapcase, title
-a = "!!!Harry!! !!!!!!!!! Harry      "
+a = "anwar !!!harry!! !!!!!!!!! harry      "
+b = "12shaka"
 
 print(a.upper())
-print("________")
+
 print(a.lower())
-print("________")
+
 print(a.rstrip("!"))
-print("________")
+
+print(a.replace("harry", "hamza"))
+
+print(a.split("!"))
+
+print(a.capitalize())
+
+print(a.center(50, "-"))
+
+print(a.count("!"))
+
+print(a.endswith("harry"))
+
+print(a.find("harry"))
+
+print(a.isalnum())
+
+print(b.isalnum())
