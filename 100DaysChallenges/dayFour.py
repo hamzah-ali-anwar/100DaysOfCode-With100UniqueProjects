4146474516565bec8a49551fc7589c1340fab9e3
Hamzah <hamzalianwar@hotmail.com>
2025-01-13 22:00:03 -0500

    Day 4 challenge

---
+++ 100DaysChallenges/dayFour.py
@@ -0,0 +1,11 @@
+# Strings are immutable
+# upper, lower, rstrip, replace, split, capitalize, center, count, endwith, find, isalnum, isalpha, islower,
+# isprintable, isspace, istitle, startswith, swapcase, title
+a = "!!!Harry!! !!!!!!!!! Harry      "
+
+print(a.upper())
+print("________")
+print(a.lower())
+print("________")
+print(a.rstrip("!"))
+print("________")
