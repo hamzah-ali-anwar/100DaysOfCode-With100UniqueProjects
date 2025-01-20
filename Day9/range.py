01a6600e24113677aca08b134a446cc645879866
Hamzah <hamzalianwar@hotmail.com>
2025-01-19 23:33:31 -0500

    python practice continues

---
+++ Day9/range.py
@@ -0,0 +1,17 @@
+# range (start, stop, step)
+
+# stop only 
+for i in range(5): # 0, 1, 2, 3, 4
+    print(i)
+
+print("____")
+
+# start and stop
+for i in range(1, 6): # 1, 2, 3, 4, 5
+    print(i)
+
+print("____")
+
+# start, stop, step
+for i in range(1, 20, 3): # 1, 4, 7, 10, 13, 16, 19
+    print(i)
