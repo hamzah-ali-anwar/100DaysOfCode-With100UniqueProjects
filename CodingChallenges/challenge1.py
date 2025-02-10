670ff88544c816b72b24264310bd9ad9c6fbc4bc
Hamzah <hamzalianwar@hotmail.com>
2025-02-10 18:10:48 -0500

    PCEP prep

--- CodingChallenges/challenge1.py
+++ CodingChallenges/challenge1.py
@@ -8,19 +8,3 @@
 #  0 < price of a product < 100000
 #  0 < number of products < 100 '''
 
-def calculateTotalPrice(prices: list[int], discount: int) -> int:
-    if not prices or discount < 0 or discount > 100:
-        return 0
-    
-    most_expensive = max(prices)
-    least_expensive = min(prices)
-
-    discount_amount = (most_expensive + least_expensive) * (discount / 100)
-
-    total_price = sum(prices) - discount_amount
-
-    return int(total_price)
-
-prices = [100, 200, 300, 40, 60, 400]
-discount = 20
-print(calculateTotalPrice(prices, discount))
---
+++ PCEP/basic.py
@@ -0,0 +1,6 @@
+# Print "Hello, PCEP!" to the console.
+print("Hello, PCEP!")
+
+# Assign the value 42 to a variable and print it.
+num = 42
+print(num)
