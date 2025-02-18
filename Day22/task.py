cf94e69745b69c71bd355aee3990a3e72ca2ab98
Hamzah <hamzalianwar@hotmail.com>
2025-02-17 19:47:37 -0500

    Day 22 initial challenges

---
+++ Day22/task.py
@@ -0,0 +1,19 @@
+# Set
+s = {2, 4, 2, 7}
+print(s)
+
+# Set is an unordered collection
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+# Empty set
+harry = set()
+print(type(harry))
+
+# Empty dictionary
+wiki = {}
+print(type(wiki))
+
+# Using info in for loop
+for value in info:
+    print(value)
