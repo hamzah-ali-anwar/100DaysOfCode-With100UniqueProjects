70be8b034e2a2d9c9e2aa127d07df815a645ce55
Hamza Anwar <39665985+hamzah-ali-anwar@users.noreply.github.com>
2025-02-22 23:01:33 -0500

    PCEP continues

---
+++ PCEP/controlFlow&Loops.py
@@ -0,0 +1,19 @@
+# Write an if statement that prints "Positive" if a number is greater than 0.
+
+# number = int(input("Please enter a positive number: "))
+
+# if number > 0:
+#     print(f"{number} is positive")
+# else:
+#     print("Please enter a number grater than zero")
+
+# print("_________")
+
+# Write an if-else statement to check if a number is even or odd.
+
+even_odd_number = int(input("Please enter a number to check if its even or odd: "))
+
+if even_odd_number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
