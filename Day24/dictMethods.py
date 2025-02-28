1b23b745b589eb7c5b0e72cbfb25e46fee52ebb0
Hamza Anwar <39665985+hamzah-ali-anwar@users.noreply.github.com>
2025-02-27 23:01:44 -0500

    Day 24 challenges

--- Day24/dictMethods.py
+++ Day24/dictMethods.py
@@ -8,3 +8,17 @@ print("____")
 
 manager_one.update(manager_two)
 print(manager_one)
+
+print("____")
+
+# manager_one.clear()
+# print(manager_one)
+
+# manager_one.pop(122)
+# print(manager_one)
+
+manager_one.popitem()
+print(manager_one)
+
+del manager_one[122]
+print(manager_one)
