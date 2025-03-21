cab434c0a93089b685a986327d4869b94e151926
Hamza Anwar <39665985+hamzah-ali-anwar@users.noreply.github.com>
2025-03-21 12:01:20 -0400

    Day 29 challenges completed

--- Day29/quizGame.py
+++ Day29/quizGame.py
@@ -2,39 +2,53 @@ print("Welcome to my computer quiz game!") # simple print msg
 
 playing = input("Do you want to play? ") # taking user's input
 
-if playing != "yes": # if user's response is anything other then yes, then program quits
+if playing.lower() != "yes": # if user's response is anything other then yes, then program quits
     quit()
 
 print("Let's play :)") # will appear if user responds yes to above question
 
+score = 0
+
 answer = input("What does CPU stands for? ") # first question
 
 # if, else statement to verify users response 
-if answer == "central processing unit":
+correct_answer = "central processing unit"
+if answer.lower() == correct_answer:
     print("Correct")
+    score += 1
 else:
-    print("Incorrect")
+    print(f"Wrong! correct answer is {correct_answer}")
 
 answer = input("What does GPU stands for? ") #  second question
 
 # if, else statement to verify users response 
-if answer == "graphics processing unit":
+correct_answer = "graphics processing unit"
+
+if answer.lower() == correct_answer:
     print("Correct")
+    score += 1
 else:
-    print("Incorrect")
+    print(f"Wrong! correct answer is {correct_answer}")
 
 answer = input("What does RAM stands for? ") # third question
 
 # if, else statement to verify users response 
-if answer == "random access memory":
+correct_answer = "random access memory"
+if answer.lower() == correct_answer:
     print("Correct")
+    score += 1
 else:
-    print("Incorrect")
+    print(f"Wrong! correct answer is {correct_answer}")
 
 answer = input("What does PSU stands for? ") # fourth question
 
 # if, else statement to verify users response 
-if answer == "power supply":
+correct_answer = "power supply"
+if answer.lower() == correct_answer:
     print("Correct")
+    score += 1
 else:
-    print("Incorrect")
+    print(f"Wrong! correct answer is {correct_answer}")
+
+print(f"You got {score} questions correct")
+print(f"You scored {round((score / 4) * 100)}%")
