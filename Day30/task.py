a2f2d5044d247daa480317d997026431d1b2e07f
Hamza Anwar <39665985+hamzah-ali-anwar@users.noreply.github.com>
2025-03-24 13:07:02 -0400

    Day 30 challenges completed

--- Day30/task.py
+++ Day30/task.py
@@ -10,3 +10,33 @@
 # If the word contains less than 3 characters, reverse it, else remove
 # 3 random characters from start and end. Now remove the last letter and
 # append it to the beginning
+
+import random # to pick random letters
+import string # to get all upper/lowercase letters
+
+def encode(word): # define function
+    if len(word) < 3:
+        return word[::-1]
+    
+    first_letter = word[0]
+    new_word = word[1:] + first_letter
+
+    random_chars = ''.join(random.choices(string.ascii_letters, k=3))
+    return random_chars + new_word + random_chars
+
+
+def decode(encoded_word):
+    if len(encoded_word) < 3:
+        return encoded_word[::-1]
+    
+    core_word = encoded_word[3:-3]
+    last_letter = core_word[-1]
+    return last_letter + core_word[:-1]
+
+original_word = "hello"
+encoded_word = encode(original_word)
+decoded_word = decode(encoded_word)
+
+print(f"Original: {original_word}")
+print(f"Encoded: {encoded_word}")
+print(f"Decoded: {decoded_word}")
